997cc01f4bf8ea0b59e2e37a71a0ab8d0372c584
Alex <alexmunoz91@gmail.com>
2021-08-30 10:56:16 +0200

    Add user model

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,15 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  picture: { type: String, default: 'http://placehold.it/32x32' },
+  age: Number,
+  name: String,
+  surname: String,
+  email: String,
+  password: String,
+  gender: String,
+  reservations: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  calendar: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+module.exports = model('User', userSchema);
