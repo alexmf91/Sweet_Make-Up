e03eab156e7cfa3c82fefc9733c0dbd445ab20f2
Alex <alexmunoz91@gmail.com>
2021-08-30 16:49:35 +0200

    Add cart and booking routes configuration

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,13 @@ server.use(express.json());
 
 const userRouter = require('./src/routes/userRoutes');
 const serviceRouter = require('./src/routes/serviceRoutes');
+const cartRouter = require('./src/routes/cartRoutes');
+const bookingRouter = require('./src/routes/bookingRoutes');
 
 server.use('/api', userRouter);
 server.use('/api', serviceRouter);
+server.use('/api', cartRouter);
+server.use('/api', bookingRouter);
 
 server.listen(
   port,
