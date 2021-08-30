f2891f134b1f1074aa46b0abe96b0991ca35f71c
Alex <alexmunoz91@gmail.com>
2021-08-30 16:49:07 +0200

    Add booking controller

---
+++ backend/src/controllers/bookingController.js
@@ -0,0 +1,27 @@
+const Booking = require('../models/bookingModel');
+
+async function createOne({ body }, res) {
+  try {
+    const newBooking = await Booking.create(body);
+    res.json(newBooking);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function getAll({ query }, res) {
+  try {
+    const bookings = await Booking.find(query)
+      .populate('services');
+    res.json(bookings);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+module.exports = {
+  createOne,
+  getAll
+};
