4743c5daecc5095b4c3d58ed25f80c51b1ee522f
Alex <alexmunoz91@gmail.com>
2021-08-31 11:53:58 +0200

    Add more cartId routes - Add getOneById method -Add updateOneById method -Add updateOneById method

--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -4,6 +4,12 @@ const cartController = require('../controllers/cartController');
 const cartRouter = new Router();
 
 cartRouter
+  .route('/cart/:cartId')
+  .put(cartController.updateOneById)
+  .get(cartController.getOneById)
+  .delete(cartController.deleteOneById);
+
+cartRouter
   .route('/cart/')
   .post(cartController.createOne)
   .get(cartController.getAll);
