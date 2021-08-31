aebe03b179260e9a37bd0c23eaabf65ac22c9510
Alex <alexmunoz91@gmail.com>
2021-08-31 11:54:10 +0200

    Add more bookingId routes - Add getOneById method -Add updateOneById method -Add updateOneById method

--- backend/src/routes/bookingRoutes.js
+++ backend/src/routes/bookingRoutes.js
@@ -4,6 +4,12 @@ const bookingController = require('../controllers/bookingController');
 const bookingRouter = new Router();
 
 bookingRouter
+  .route('/booking/:bookingId')
+  .put(bookingController.updateOneById)
+  .get(bookingController.getOneById)
+  .delete(bookingController.deleteOneById);
+
+bookingRouter
   .route('/booking/')
   .post(bookingController.createOne)
   .get(bookingController.getAll);
