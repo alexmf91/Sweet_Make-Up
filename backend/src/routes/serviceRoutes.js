3076e44931c55788d6a07b2b9c86ebb21ba9a89d
Alex <alexmunoz91@gmail.com>
2021-08-31 11:54:27 +0200

    Add more serviceId routes - Add getOneById method -Add updateOneById method -Add updateOneById method

--- backend/src/routes/serviceRoutes.js
+++ backend/src/routes/serviceRoutes.js
@@ -4,6 +4,12 @@ const serviceController = require('../controllers/serviceController');
 const serviceRouter = new Router();
 
 serviceRouter
+  .route('/service/:serviceId')
+  .put(serviceController.updateOneById)
+  .get(serviceController.getOneById)
+  .delete(serviceController.deleteOneById);
+
+serviceRouter
   .route('/service/')
   .post(serviceController.createOne)
   .get(serviceController.getAll);
