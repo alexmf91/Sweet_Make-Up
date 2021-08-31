310438c5dbbf306024cdd08a2d048c5b11746cfc
Alex <alexmunoz91@gmail.com>
2021-08-31 11:53:20 +0200

    Add more userId routes - Add getOneById method -Add updateOneById method -Add updateOneById method

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,6 +4,12 @@ const userController = require('../controllers/userController');
 const userRouter = new Router();
 
 userRouter
+  .route('/user/:userId')
+  .put(userController.updateOneById)
+  .get(userController.getOneById)
+  .delete(userController.deleteOneById);
+
+userRouter
   .route('/users/')
   .post(userController.createOne)
   .get(userController.getAll);
