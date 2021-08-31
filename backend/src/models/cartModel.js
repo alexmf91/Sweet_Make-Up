2d6150a0f02a071a157148cd8a9a08548ac81307
Alex <alexmunoz91@gmail.com>
2021-08-31 11:55:22 +0200

    Add more propertys: - Add types property - Add totalprice property

--- backend/src/models/cartModel.js
+++ backend/src/models/cartModel.js
@@ -4,8 +4,10 @@ const cartSchema = new Schema({
   services: [
     {
       service: { type: Schema.Types.ObjectId, ref: 'Service' },
-      amount: Number
-    }]
+      amount: Number,
+      types: String
+    }],
+  totalPrice: Number
 });
 
 module.exports = model('Cart', cartSchema);
