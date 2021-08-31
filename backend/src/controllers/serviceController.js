62c50c3d6c4f1bbc2a5af2806c1cf56e2cbcd83b
Alex <alexmunoz91@gmail.com>
2021-08-31 11:52:06 +0200

    Add more functions: - Add getOneById function -Add updateOneById function -Add updateOneById function

--- backend/src/controllers/serviceController.js
+++ backend/src/controllers/serviceController.js
@@ -20,7 +20,42 @@ async function getAll({ query }, res) {
   }
 }
 
+async function getOneById({ params: { serviceId } }, res) {
+  try {
+    const services = await Service.findById(serviceId);
+    res.json(services);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function updateOneById({ body, params: { serviceId } }, res) {
+  try {
+    const updatedservice = await Service.findByIdAndUpdate(serviceId,
+      body, { new: true, useFindAndModify: false });
+    res.json(updatedservice);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
+async function deleteOneById({ params: { serviceId } }, res) {
+  try {
+    await Service.findByIdAndDelete(serviceId);
+    res.status(204);
+    res.send();
+  } catch (error) {
+    res.status(500);
+    res.send(error);
+  }
+}
+
 module.exports = {
   createOne,
-  getAll
+  getAll,
+  getOneById,
+  updateOneById,
+  deleteOneById
 };
