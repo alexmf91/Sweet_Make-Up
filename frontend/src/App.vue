1eb47530877a18cc27cf381c407eaf131ec29fd9
Alex <alexmunoz91@gmail.com>
2021-09-03 17:20:18 +0200

    Import header and footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div id='app'>
+  <app-header></app-header>
+  <router-view class='container'></router-view>
+  <app-footer></app-footer>
+</div>
 </template>
 
-<style>
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  name: 'app',
+  components: { AppHeader, AppFooter },
+};
+</script>
+
+<style lang='scss'>
+@import "styles/reset.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0 auto;
+  padding: 0;
+}
+.container {
+  padding-bottom: 4rem;
 }
 
 #nav {
