933c49f147e7e468b0d15ad65ae985b86c5935a7
Alex <alexmunoz91@gmail.com>
2021-09-04 13:16:07 +0200

    Add fetch from api functionality

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,21 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    services: [],
   },
   mutations: {
+    loadServices(state, payload) {
+      state.services = payload;
+    },
   },
   actions: {
+    async fetchServicesFromApi({ commit }) {
+      const { data } = await axios.get('http://localhost:8001/api/service');
+      console.log(data);
+      commit('loadServices', data);
+    },
   },
   modules: {
   },
