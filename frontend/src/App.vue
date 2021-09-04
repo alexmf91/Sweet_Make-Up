81b759af5ee3d3dcb3385a6dbf0b19d5a2c71d6a
Alex <alexmunoz91@gmail.com>
2021-09-04 13:20:26 +0200

    Modify font family

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,10 +18,9 @@ export default {
 
 <style lang='scss'>
 @import "styles/reset.scss";
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family:'Josefin Sans', sans-serif;
   text-align: center;
   color: #2c3e50;
   margin: 0 auto;
