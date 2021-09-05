af274bc6c9e9560bfa6a573d892a803a690ca552
Alex <alexmunoz91@gmail.com>
2021-09-05 15:20:49 +0200

    Change AppServices component  tag

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,22 +2,25 @@
   <div class='home'>
     <AppSweetBanner />
     <AppAboutBrand />
-      <app-services></app-services>
+    <AppServices />
   </div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
-import AppServices from '../components/Services.vue';
 import AppSweetBanner from '../components/AppSweetBanner.vue';
 import AppAboutBrand from '../components/AppAboutBrand.vue';
+import AppServices from '../components/AppHomeServices.vue';
 
 export default defineComponent({
   name: 'Home',
-  components: { AppServices, AppSweetBanner, AppAboutBrand },
+  components: {
+    AppSweetBanner,
+    AppAboutBrand,
+    AppServices,
+  },
 });
 </script>
 
 <style lang='scss' scoped>
-
 </style>
