7363c683d37a5cdec04f04e966bef52aa5989e01
Alex <alexmunoz91@gmail.com>
2021-09-05 15:25:33 +0200

    Add service view

---
+++ frontend/src/views/Service.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ name }}</h1>
+    <CategoryServices />
+  </div>
+</template>
+
+<script>
+import CategoryServices from '../components/CategoryServices.vue';
+
+export default {
+  name: 'service-view',
+  components: {
+    CategoryServices,
+  },
+  props: {
+    id: String,
+    name: String,
+    category: String,
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.service {
+  background-color: lightgreen;
+}
+</style>
