3230203b28a3bf4dd78baec9017d8512b29b78f5
Alex <alexmunoz91@gmail.com>
2021-09-05 15:23:05 +0200

    Add categoryServices component

---
+++ frontend/src/components/CategoryServices.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="category-services">
+    <h2>Category Services Component</h2>
+    <h3>{{ name }}</h3>
+    <ul>
+      <li v-for="service in categoryServices" :key="service.name">
+        <router-link
+          class="service-card"
+          v-bind:to="'service/' + currentCategory + '/' + service.name"
+          >
+          <p>{{ service.name }}</p>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+s
+<script lang='ts'>
+import { defineComponent } from "vue";
+import { mapState, mapActions } from "vuex";
+import { useRoute } from "vue-router";
+
+export default defineComponent({
+  name: "CategoryServices",
+  computed: {
+    ...mapState(["categoryServices", "currentCategory"]),
+  },
+  methods: {
+    ...mapActions(["fetchServicesForCategoryFromApi"]),
+  },
+  mounted() {
+    const route = useRoute();
+    const { category } = route.params;
+    this.fetchServicesForCategoryFromApi(category);
+  },
+});
+</script>
+
+<style lang='scss' scoped>
+ul {
+  background-color: violet;
+  li {
+    background-color: wheat;
+  }
+}
+</style>
