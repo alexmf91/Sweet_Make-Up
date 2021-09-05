de9659d86a65bcb875f0e06de62af2e0d7e13abf
Alex <alexmunoz91@gmail.com>
2021-09-05 15:22:19 +0200

    Add fect from category functionality

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -4,17 +4,29 @@ import axios from 'axios';
 export default createStore({
   state: {
     services: [],
+    categoryServices: [],
+    currentCategory: '',
   },
   mutations: {
     loadServices(state, payload) {
       state.services = payload;
     },
+    loadCategoryServices(state, payload) {
+      state.categoryServices = payload;
+    },
+    changeCurrentCategory(state, payload) {
+      state.currentCategory = payload;
+    },
   },
   actions: {
     async fetchServicesFromApi({ commit }) {
       const { data } = await axios.get('http://localhost:8001/api/service');
-      console.log(data);
       commit('loadServices', data);
+    },
+    async fetchServicesForCategoryFromApi({ commit }, query) {
+      const { data } = await axios.get(`http://localhost:8001/api/service/?type=${query}`);
+      commit('loadCategoryServices', data);
+      commit('changeCurrentCategory', query);
     },
   },
   modules: {
