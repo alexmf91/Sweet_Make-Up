6488b2539bf0419f14df53f69d15e5c5ac6f45ac
Alex <alexmunoz91@gmail.com>
2021-09-05 15:19:49 +0200

    Add serviceCard

---
+++ frontend/src/components/ServiceCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <router-link
+      class='service-card'
+      v-bind:to="{ name: 'service', params: { name, id, category } }"
+    >
+    <h2>{{name}}</h2>
+    </router-link>
+</template>
+
+<script>
+export default {
+  name: 'service-card',
+  props: {
+    id: String,
+    name: String,
+    category: String,
+  },
+};
+</script>
