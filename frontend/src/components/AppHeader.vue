3eeb90fb747d5ff929ad850bbcc4d57171bbc1e7
Alex <alexmunoz91@gmail.com>
2021-09-06 16:32:36 +0200

    Add user routes funcionality

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -3,7 +3,7 @@
     <router-link class="btn-header button" to="/">
       <img class="main-logo" src="../assets/main-logo.svg" alt="" />
     </router-link>
-    <div class="desktop-nav-links">
+    <nav class="desktop-nav-links">
       <router-link class="btn-header button" to="/">Home</router-link>
       <router-link class="btn-header button" to="/Profile"
         >Nosotros</router-link
@@ -11,11 +11,19 @@
       <router-link class="btn-header button" to="/">Servicios</router-link>
       <router-link class="btn-header button" to="/">Reservas</router-link>
       <router-link class="btn-header button" to="/">Contacto</router-link>
-    </div>
-    <div class="mobile-nav-links">
-      <span class="user-icon button">
-        <i class="fas fa-user fa-lg"></i>
-      </span>
+    </nav>
+    <nav class="mobile-nav-links">
+      <div class="mobile-nav-links__user">
+        <span class="user-icon">
+          <i class="fas fa-user fa-lg"></i>
+        </span>
+        <div class="user-icon__content">
+          <router-link to="/profile">Profile</router-link>
+          <router-link to="/">Log out</router-link>
+          <router-link to="/profile">Log in</router-link>
+          <router-link to="/register">Sign Up</router-link>
+        </div>
+      </div>
       <span class="cart-icon button">
         <i class="fas fa-shopping-cart fa-lg"></i>
       </span>
@@ -25,19 +33,21 @@
         </span>
         <div class="burger-menu__content">
           <router-link class="btn-header button" to="/">Home</router-link>
-          <router-link class="btn-header button" to="/Profile">Nosotros</router-link>
+          <router-link class="btn-header button" to="/Profile"
+            >Nosotros</router-link
+          >
           <router-link class="btn-header button" to="/">Servicios</router-link>
           <router-link class="btn-header button" to="/">Reservas</router-link>
           <router-link class="btn-header button" to="/">Contacto</router-link>
         </div>
       </div>
-      <div class='search-bar'>
+      <div class="search-bar">
         <span class="search-bar__icon button">
           <i class="fas fa-search fa-lg"></i>
         </span>
-        <input class='search-bar__bar' type="text" placeholder="Search">
+        <input class="search-bar__bar" type="text" placeholder="Search" />
       </div>
-    </div>
+    </nav>
   </header>
 </template>
 
@@ -51,7 +61,7 @@ export default {
 @import "../styles/reset.scss";
 @import "../styles/colors.scss";
 header {
-  background-color: lightgray;
+  background-color: rgb(223, 215, 215);
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -82,6 +92,10 @@ button {
     text-decoration: none;
     padding: 0.1rem;
     margin: 0 0.2rem;
+    &:hover {
+      color: $secondary-color;
+      font-weight: 600;
+    }
   }
 }
 
@@ -94,12 +108,47 @@ button {
     color: $secondary-color;
   }
 }
+
+.mobile-nav-links__user {
+  position: relative;
+}
+
+.user-icon__content {
+  display: none;
+  right: 15px;
+  text-decoration: none;
+  padding-top: 4px;
+  padding: 15px;
+  position: absolute;
+  top: 100%;
+  background-color: #fff;
+  z-index: 9999;
+  height: fit-content;
+  width: 100px;
+  a {
+    text-decoration: none;
+    padding: 3px 0;
+    color:$quartiary-color;
+    &:hover{
+      color:$tertiary-color;
+    }
+  }
+  &:hover {
+    display: flex;
+    flex-direction: column;
+  }
+}
+.mobile-nav-links__user:hover .user-icon__content {
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+}
 .burger-menu {
   position: relative;
 }
 
 .burger-menu__content {
-  display:none;
+  display: none;
   right: 10px;
   text-decoration: none;
   padding-top: 4px;
@@ -109,17 +158,16 @@ button {
   z-index: 9999;
   height: 200px;
   width: 120px;
-  a{
+  a {
     text-decoration: none;
-    color:$quartiary-color;
-    &:hover{
-    color:$secondary-color;
+    color: $quartiary-color;
+    &:hover {
+      color: $secondary-color;
     }
   }
-
 }
 .burger-menu:hover .burger-menu__content {
-  display:flex;
+  display: flex;
   flex-direction: column;
   text-decoration: none;
 }
@@ -128,8 +176,8 @@ button {
   position: relative;
 }
 
-.search-bar__bar{
-  display:none;
+.search-bar__bar {
+  display: none;
   border-radius: 5rem;
   right: 0.5rem;
   text-decoration: none;
@@ -141,12 +189,14 @@ button {
   height: 30px;
   width: 120px;
   outline: none;
-
 }
 .search-bar:hover .search-bar__bar {
-  display:flex;
+  display: flex;
   flex-direction: column;
   text-decoration: none;
+}
+i {
+  font-family: "Font Awesome 5 Free";
 }
 @media screen and (max-width: 480px) {
   .desktop-nav-links,
@@ -157,7 +207,7 @@ button {
   }
 }
 @media screen and (min-width: 480px) {
-  .burger-menu{
+  .burger-menu {
     display: none;
   }
 }
