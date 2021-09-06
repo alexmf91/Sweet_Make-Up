1d78eaf2e433dd383b0a143ec4d51d611af0c4a2
Alex <alexmunoz91@gmail.com>
2021-09-06 16:31:03 +0200

    Add register component

---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class='register-view'>
+    <h2>Register</h2>
+    <RegisterForm />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import RegisterForm from "../components/RegisterForm.vue";
+
+export default defineComponent({
+  name: "Register",
+  components: {
+    RegisterForm,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/reset.scss";
+@import "../styles/colors.scss";
+
+.register-view{
+    background-color: $primary-color;
+}
+</style>
