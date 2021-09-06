4431bb5af21b64b5a4f552962509e996068dbbc9
Alex <alexmunoz91@gmail.com>
2021-09-06 16:31:59 +0200

    Add categroy params

--- frontend/src/components/ServiceCard.vue
+++ frontend/src/components/ServiceCard.vue
@@ -1,7 +1,7 @@
 <template>
     <router-link
       class='service-card'
-      v-bind:to="{ name: 'service', params: { name, id, category } }"
+      :to="{ name: 'service', params: { name, id, category } }"
     >
     <h2>{{name}}</h2>
     </router-link>
