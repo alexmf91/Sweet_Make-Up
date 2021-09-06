7bb5f9cc567dec1aab3c0bda23c72f8f3962dec0
Alex <alexmunoz91@gmail.com>
2021-09-06 16:30:56 +0200

    Add service details component

---
+++ frontend/src/views/ServiceDetails.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Hola panitas Aqui estan vuestros current services</h1>
+    <h3>{{ currentService.name }}</h3>
+    <p>{{ currentService.description }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { mapState, mapActions } from 'vuex';
+import { useRoute } from 'vue-router';
+
+export default defineComponent({
+  name: 'ServiceDetails',
+  computed: {
+    ...mapState(['currentService']),
+  },
+  methods: {
+    ...mapActions(['fetchOneServiceForNameFromApi']),
+  },
+  mounted() {
+    const route = useRoute();
+    const { name } = route.params;
+    console.log(name);
+    this.fetchOneServiceForNameFromApi(name);
+  },
+});
+</script>
