6e5c945622ac00bc82a38f9aed99a0ef3ccb1020
Alex <alexmunoz91@gmail.com>
2021-09-07 10:17:57 +0200

    Add login user functionality

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex';
 import axios from 'axios';
+import State from '../types/interface';
 
 export default createStore({
   state: {
@@ -7,19 +8,23 @@ export default createStore({
     categoryServices: [],
     currentCategory: '',
     currentService: {},
+    currentUser: "",
   },
   mutations: {
-    loadServices(state, payload) {
+    loadServices(state: State, payload) {
       state.services = payload;
     },
-    loadCategoryServices(state, payload) {
+    loadCategoryServices(state: State, payload) {
       state.categoryServices = payload;
     },
-    changeCurrentCategory(state, payload) {
+    changeCurrentCategory(state: State, payload) {
       state.currentCategory = payload;
     },
-    loadOneService(state, payload) {
+    loadOneService(state: State, payload) {
       state.currentService = payload;
+    },
+    loadUser(state: State, payload) {
+      state.currentUser = payload;
     },
   },
   actions: {
@@ -27,18 +32,22 @@ export default createStore({
       const { data } = await axios.get('http://localhost:8001/api/service');
       commit('loadServices', data);
     },
-    async fetchServicesForCategoryFromApi({ commit }, query) {
+    async fetchServicesForCategoryFromApi({ commit }, query: string) {
       const { data } = await axios.get(`http://localhost:8001/api/service/?type=${query}`);
       commit('loadCategoryServices', data);
       commit('changeCurrentCategory', query);
     },
-    async fetchOneServiceForNameFromApi({ commit }, query) {
+    async fetchOneServiceForNameFromApi({ commit }, query: string) {
       const { data } = await axios.get(`http://localhost:8001/api/service/?name=${query}`);
       commit('loadOneService', data[0]);
     },
     async registerUser({ commit }, user) {
       await axios.post('http://localhost:8001/api/register', user);
       commit("");
+    },
+    async loginUser({ commit }, user) {
+      const { data } = await axios.post('http://localhost:8001/api/login', user);
+      commit("loadUser", data.user);
     },
   },
   modules: {
