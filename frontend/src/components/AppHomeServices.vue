50ec5bc9413c4245bec8d1f0f17e16ca62c9cf7f
Alex <alexmunoz91@gmail.com>
2021-09-07 10:13:36 +0200

    Add service protorype img

--- frontend/src/components/AppHomeServices.vue
+++ frontend/src/components/AppHomeServices.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h2>Services</h2>
-    <div class='app-services'>
+    <ServicesHomeBanner />
+    <div class="app-services">
       <service-card
-        v-for='(service, index) in services'
-        :key='index'
-        :name='service.name'
-        :id='service.id'
-        :category='service.category'
+        v-for="(service, index) in services"
+        :key="index"
+        :name="service.name"
+        :id="service.id"
+        :category="service.category"
+        :image="service.image"
       >
       </service-card>
     </div>
@@ -15,25 +16,39 @@
 </template>
 
 <script>
-import ServiceCard from './ServiceCard.vue';
+import ServicesHomeBanner from "./ServicesHomeBanner.vue";
+import ServiceCard from "./ServiceCard.vue";
 
 export default {
-  name: 'home-services-component',
-  components: { ServiceCard },
+  name: "home-services-component",
+  components: {
+    ServiceCard,
+    ServicesHomeBanner,
+  },
   data() {
     return {
       services: [
         {
-          id: '1',
-          name: 'Maquillaje y peinados para eventos',
-          category: 'beauty_for_brides',
+          id: "1",
+          name: "Maquillaje y peinados para eventos",
+          category: "make_up_and_hairstyles",
+          image:
+            "https://i.ibb.co/9v1VrS4/Here-Comes-The-Bride-45-Wedding-Hairstyles-for-All-Hair-Lengths.jpg",
         },
         {
-          id: '2',
-          name: 'Belleza para novias',
-          category: 'make_up_and_hairstyles',
+          id: "2",
+          name: "Belleza para novias",
+          category: "beauty_for_brides",
+          image:
+            "https://i.ibb.co/9v1VrS4/Here-Comes-The-Bride-45-Wedding-Hairstyles-for-All-Hair-Lengths.jpg",
         },
-        { id: '3', name: 'Beauty corner', category: 'beauty_corner' },
+        {
+          id: "3",
+          name: "Beauty corner",
+          category: "beauty_corner",
+          image:
+            "https://i.ibb.co/9v1VrS4/Here-Comes-The-Bride-45-Wedding-Hairstyles-for-All-Hair-Lengths.jpg",
+        },
       ],
     };
   },
