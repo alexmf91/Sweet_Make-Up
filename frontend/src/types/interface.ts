6768feefa70403785fc32983040d88b8e93ac8b4
Alex <alexmunoz91@gmail.com>
2021-09-07 10:18:10 +0200

    Add ts state types

---
+++ frontend/src/types/interface.ts
@@ -0,0 +1,9 @@
+interface State {
+    services: Object ,
+    categoryServices: Object,
+    currentCategory: String,
+    currentService: Object,
+    currentUser: String | Object,
+}
+
+export default State;
