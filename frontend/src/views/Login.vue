35fa779849b5e306f0d50289bcb738efb261f89f
Alex <alexmunoz91@gmail.com>
2021-09-08 08:46:54 +0200

    Add login view

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="register-view">
+    <h2>login</h2>
+    <LogInForm />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import LogInForm from "../components/LogInForm.vue";
+
+export default defineComponent({
+  name: "Login",
+  components: {
+    LogInForm,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/reset.scss";
+@import "../styles/colors.scss";
+
+.register-view {
+  background-color: $primary-color;
+}
+</style>
