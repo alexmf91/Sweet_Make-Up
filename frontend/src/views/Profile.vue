1feb103470714a59b7a37709573c3976451cec8c
Alex <alexmunoz91@gmail.com>
2021-09-08 08:47:20 +0200

    Add user information and first styles

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,12 +1,39 @@
 <template>
-<div>
-  <h1>This is the profile</h1>
-</div>
+  <div class="profile">
+    <UserCard
+      :name="currentUser.name"
+      :surname="currentUser.surname"
+      :picture="currentUser.picture"
+      :email="currentUser.email"
+    />
+    <SearchBar />
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import UserCard from "../components/UserCard.vue";
+import SearchBar from "../components/SearchBar.vue";
 
 export default {
-  name: 'Profile',
+  name: "Profile",
+  components: {
+    SearchBar,
+    UserCard,
+  },
+  computed: {
+    ...mapState(["currentUser"]),
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+.profile {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  background-color: wheat;
+  padding: 4rem 0;
+}
+</style>
