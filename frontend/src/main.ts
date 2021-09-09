6562bf6dd8d095487dad93da41ae915ba9be18c9
Alex <alexmunoz91@gmail.com>
2021-09-09 17:48:45 +0200

    Install toast

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from 'vue';
+import DKToast from 'vue-dk-toast';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+createApp(App)
+  .use(DKToast, {
+    duration: 4000,
+    positionY: 'bottom',
+    positionX: 'right',
+    disableClick: true,
+    styles: {
+      color: '#000',
+      backgroundColor: '#fff',
+    },
+    max: 10,
+  }).use(store).use(router)
+  .mount('#app');
