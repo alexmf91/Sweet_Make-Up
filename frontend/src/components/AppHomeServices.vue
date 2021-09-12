eaa2f9fcccd23de3859fc54bf34ac95c5e337c01
Alex <alexmunoz91@gmail.com>
2021-09-12 16:50:37 +0200

    Add description property

--- frontend/src/components/AppHomeServices.vue
+++ frontend/src/components/AppHomeServices.vue
@@ -1,5 +1,5 @@
 <template>
-  <section id='services'>
+  <section class="services" id="services">
     <ServicesHomeBanner />
     <div class="app-services">
       <service-card
@@ -9,6 +9,7 @@
         :id="service.id"
         :category="service.category"
         :image="service.image"
+        :description="service.description"
       >
       </service-card>
     </div>
@@ -33,7 +34,9 @@ export default {
           name: "Maquillaje y peinados para eventos",
           category: "make_up_and_hairstyles",
           image:
-            "https://i.ibb.co/9v1VrS4/Here-Comes-The-Bride-45-Wedding-Hairstyles-for-All-Hair-Lengths.jpg",
+            "https://4.bp.blogspot.com/-r72nYOtaH5c/WjqI5DEn1kI/AAAAAAAAKJU/XYTvBoSaccsBt4GBR-KaWysXYXQWMpqmACLcBGAs/s1600/iStock-177511535.jpg",
+          description:
+            "Si madrinas o damas de honor, te acompañaran en los preparativos y quieren que les maquillemos, disponemos de un equipo el cual se hará cargo de dejarlas increíbles para tu gran día.",
         },
         {
           id: "2",
@@ -41,16 +44,26 @@ export default {
           category: "beauty_for_brides",
           image:
             "https://i.ibb.co/9v1VrS4/Here-Comes-The-Bride-45-Wedding-Hairstyles-for-All-Hair-Lengths.jpg",
+          description:
+            "Déjate asesorar por nosotras y juntas conseguiremos el look perfecto para que luzcas increíble en el día de tu boda.  Tras reunirnos para conocer tu estilo y preferencias, estudiaremos tu morfología y  encontraremos juntas tu maquillaje perfecto.",
         },
         {
           id: "3",
           name: "Beauty corner",
           category: "beauty_corner",
           image:
-            "https://i.ibb.co/9v1VrS4/Here-Comes-The-Bride-45-Wedding-Hairstyles-for-All-Hair-Lengths.jpg",
+            "https://1.bp.blogspot.com/-DR83zWxUoQw/V42SiCmHIzI/AAAAAAAAFp4/LafPi58w72U9agN4POWoTpHOTXZNrBFBQCLcB/s1024/beauty-corner.jpg",
+          description:
+            "Si quieres lucir radiante durante toda tu boda, o cambiar de look para la fiesta, te ofrecemos el servicio de Beauty Corner. Un rincón de retoque de maquillaje para novias e invitadas durante todo el evento. ¡Lucirás perfecta hasta el baile!",
         },
       ],
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.services {
+  padding-bottom: 10rem;
+}
+</style>
