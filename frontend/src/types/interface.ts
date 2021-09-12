8de66fee3c61fc9917654686c9f1be850ff3e878
Alex <alexmunoz91@gmail.com>
2021-09-12 16:54:59 +0200

    Add filter states

--- frontend/src/types/interface.ts
+++ frontend/src/types/interface.ts
@@ -1,10 +1,13 @@
 interface State {
-    services: Object ,
+    services: [{price: number}] | [] ,
     categoryServices: Object,
     currentCategory: String,
     currentService: Object,
     currentUser: String | Object,
     cart: any, 
+    keyword: String,
+    selectedCategory: String,
+    selectedSortPrice: String,
 }
 
 export default State;
