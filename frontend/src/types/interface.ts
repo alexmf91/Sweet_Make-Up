0e00609789b2ba71b3fd91466d07663a9bfb8490
Alex <alexmunoz91@gmail.com>
2021-09-13 09:38:01 +0200

    Add Interfaces: - User, - RegisterUser, - LoginUser, - Service, - CartService

--- frontend/src/types/interface.ts
+++ frontend/src/types/interface.ts
@@ -1,5 +1,5 @@
-interface State {
-    services: [{price: number}] | [] ,
+export interface State {
+    services: Array <Service> | [] ,
     categoryServices: Object,
     currentCategory: String,
     currentService: Object,
@@ -10,4 +10,45 @@ interface State {
     selectedSortPrice: String,
 }
 
-export default State;
+export interface User {
+    name: String,
+    surname: String,
+    phone: String,
+    email: String,
+    password: String,
+    gender: String,
+    cart: String,
+    bookings: Array<Service>
+}
+export interface RegisterUser {
+    _id: String,
+    name: String,
+    surname: String,
+    phone: String,
+    email: String,
+    password: String,
+}
+
+export interface LoginUser {
+    name: String,
+    password: String,
+}
+
+export interface Service {
+    _id: String,
+    picture: String,
+    name: String,
+    description: String,
+    duration: String,
+    price: Number,
+    type: String,
+}
+
+export interface CartService {
+    service: Service,
+    amount: Number,
+    types: String
+}
+export interface Cart {
+    services: Array<CartService> | String,
+}
