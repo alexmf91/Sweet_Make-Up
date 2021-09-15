7619ae713b9a30bef883eef099989478fabf3e70
Alex <alexmunoz91@gmail.com>
2021-09-15 18:17:47 +0200

    Fix identation

--- frontend/tests/unit/store/state.spec.ts
+++ frontend/tests/unit/store/state.spec.ts
@@ -1,19 +1,19 @@
 import state from '@/store/state';
 
 describe('Given a state', () => {
-    describe('When is called', () => {
-        test('Should be equal to the real state', () => {
-            expect(state).toEqual({
-                services: [],
-                categoryServices: [],
-                currentCategory: '',
-                currentService: {},
-                currentUser: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user") || "") : "",
-                cart: [],
-                keyword: '',
-                selectedCategory: '',
-                selectedSortPrice: '',
-            })
-        })
-    })
-})
+  describe('When is called', () => {
+    test('Should be equal to the real state', () => {
+      expect(state).toEqual({
+        services: [],
+        categoryServices: [],
+        currentCategory: '',
+        currentService: {},
+        currentUser: localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') || '') : '',
+        cart: [],
+        keyword: '',
+        selectedCategory: '',
+        selectedSortPrice: '',
+      });
+    });
+  });
+});
