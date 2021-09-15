f2d4fb232f9cebf615d3a03b6366948d5be72b64
Alex <alexmunoz91@gmail.com>
2021-09-15 18:16:56 +0200

    Move test to components test folder

---
+++ frontend/tests/unit/components/LoginForm.spec.ts
@@ -0,0 +1,109 @@
+import { mount } from '@vue/test-utils';
+import LoginForm from '@/components/LoginForm.vue';
+import router from '@/router';
+import state from '../../mockedState';
+
+const mockRouter = {
+  push: jest.fn(),
+};
+
+describe('Given a LoginForm component', () => {
+  describe('When the component is rendered', () => {
+    test('Then should be the header html tag', () => {
+      const $store = {
+        state,
+        dispatch: jest.fn(),
+        commit: jest.fn(),
+      };
+      const methods = {
+        login: jest.fn(),
+        scrollToTop: jest.fn(),
+      };
+
+      const wrapper = mount(LoginForm, {
+        global: {
+          mocks: {
+            $router: mockRouter,
+            $store,
+            methods,
+          },
+          plugins: [router],
+        },
+      });
+      const byId = wrapper.find('#login-form');
+      expect(byId.element.id).toBe('login-form');
+    });
+    describe('And the submit button is  clicked', () => {
+      test('Then the login function should be called', async () => {
+        window.scrollTo = () => null;
+        const $store = {
+          state,
+          actions: {
+            loginUser: jest.fn(),
+          },
+          methods: {
+            login: jest.fn(),
+          },
+          dispatch: jest.fn(),
+          commit: jest.fn(),
+        };
+        const methods = {
+          login: jest.fn(),
+          scrollToTop: jest.fn(),
+        };
+
+        const wrapper = mount(LoginForm, {
+          global: {
+            mocks: {
+              $router: mockRouter,
+              $store,
+              methods,
+            },
+            plugins: [router],
+          },
+        });
+        const login = jest.fn();
+        login();
+        const loginButton = wrapper.get('[data-test="login-button"]');
+        await loginButton.trigger('submit');
+
+        expect(login).toHaveBeenCalled();
+      });
+      test('Then the login function should be called', async () => {
+        window.scrollTo = () => null;
+        const $store = {
+          state,
+          actions: {
+            loginUser: jest.fn(),
+          },
+          methods: {
+            login: jest.fn(),
+          },
+          dispatch: jest.fn(),
+          commit: jest.fn(),
+        };
+        const methods = {
+          login: jest.fn(),
+          scrollToTop: jest.fn(),
+        };
+
+        const wrapper = mount(LoginForm, {
+          global: {
+            mocks: {
+              $router: mockRouter,
+              $store,
+              methods,
+            },
+            plugins: [router],
+          },
+        });
+        const login = jest.fn();
+        login();
+        const loginButton = wrapper.get('[data-test="login-button"]');
+        await loginButton.trigger('submit');
+
+        expect(login).toHaveBeenCalled();
+      });
+    });
+  });
+});
